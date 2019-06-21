507718f3bc002e1f77d86704106bc1775cd3b694
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-06-21 12:11:57 -0400

    Now passing schedule state between components

---
+++ src/Fritz.ResourceManagement.Web/Data/ScheduleState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fritz.ResourceManagement.Domain;
+
+namespace Fritz.ResourceManagement.Web.Data
+{
+
+  public class ScheduleState
+  {
+
+	public Schedule Schedule { get; set; }
+
+	public DateTime SelectedDate { get; private set; }
+
+	public void SelectDate(DateTime newDate)
+	{
+	  SelectedDate = newDate;
+	  OnSelectedDateChanged?.Invoke(null, new SelectedDateChangedArgs() { SelectedDate = newDate });
+	}
+
+	public void ScheduleUpdated()
+	{
+	  OnSelectedDateChanged?.Invoke(null, new SelectedDateChangedArgs() { SelectedDate = SelectedDate });
+	}
+
+	public event EventHandler<SelectedDateChangedArgs> OnSelectedDateChanged;
+
+	public class SelectedDateChangedArgs : EventArgs {
+
+	  public DateTime SelectedDate { get; set; }
+
+	}
+
+
+
+  }
+
+
+}
--- src/Fritz.ResourceManagement.Web/Startup.cs
+++ src/Fritz.ResourceManagement.Web/Startup.cs
@@ -48,6 +48,8 @@ namespace Fritz.ResourceManagement.Web
 	  services.AddSignalR();
 	  services.AddServerSideBlazor();
 
+	  services.AddScoped<ScheduleState>();
+
 	  //cheer ultramark 31/05/2019 100
 
 	  services.AddHttpContextAccessor();
