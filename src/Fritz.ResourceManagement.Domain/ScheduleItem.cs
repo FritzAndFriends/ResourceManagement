8f2af12f0c10c5c875eca661a56b7e1363c6f818
Daniel Loudon <daniel_loudon@Hotmail.com>
2019-07-21 16:09:32 +0100

    Issue #8 - domain object validation (#34)
    * issue #8 inital commit
    
    * Issue #8 Cron validation in RecurringSchedule.cs
    
    * more validation rules issue #8
    
    * validation rules for default datetimes issue #8

--- src/Fritz.ResourceManagement.Domain/ScheduleItem.cs
+++ src/Fritz.ResourceManagement.Domain/ScheduleItem.cs
@@ -39,8 +39,24 @@ namespace Fritz.ResourceManagement.Domain
 
 	  var results = new List<ValidationResult>();
 
+	  if (Status == default)
+		results.Add(new ValidationResult($"{nameof(Status)} is required", new[] { nameof(Status) }));
+
+	  if (string.IsNullOrWhiteSpace(Name))
+		results.Add(new ValidationResult($"{nameof(Name)} cannot be null, empty or consist of whitespace only", new[] { nameof(Name) }));
+	  else if (Name.Length > 50)
+		results.Add(new ValidationResult($"{nameof(Name)} is greater than max length of 50", new[] { nameof(Name) }));
+
+		if(StartDateTime == default)
+		  results.Add(new ValidationResult($"{nameof(StartDateTime)} is required", new[] { nameof(StartDateTime) }));
+
+	  if (EndDateTime == default)
+			results.Add(new ValidationResult($"{nameof(EndDateTime)} is required", new[] { nameof(EndDateTime) }));
+
 	  if (EndDateTime < StartDateTime)
-			results.Add(new ValidationResult("EndDateTime cannot be before StartDateTime", new[] { nameof(StartDateTime), nameof(EndDateTime) }));
+		results.Add(new ValidationResult($"{nameof(EndDateTime)} cannot be before {nameof(StartDateTime)}", new[] { nameof(StartDateTime), nameof(EndDateTime) }));
+	  else if (EndDateTime < StartDateTime.AddMinutes(10))
+		results.Add(new ValidationResult($"{nameof(EndDateTime)} must be atleast 10 minutes after {nameof(StartDateTime)}", new[] { nameof(StartDateTime), nameof(EndDateTime) }));
 
 	  return results;
 
--- src/Fritz.ResourceManagement.Domain/TimeSlot.cs
+++ src/Fritz.ResourceManagement.Domain/TimeSlot.cs
@@ -1,16 +1,37 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Fritz.ResourceManagement.Domain
 {
-  public class TimeSlot {
+  public class TimeSlot
+  {
 
-    public DateTime StartDateTime { get; set; }
+	public DateTime StartDateTime { get; set; }
 
-    public DateTime EndDateTime { get; set; }
+	public DateTime EndDateTime { get; set; }
 
-    public ScheduleStatus Status { get; set; }
+	public ScheduleStatus Status { get; set; }
 
+	public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+	{
 
+	  var results = new List<ValidationResult>();
+
+	  if (Status == default)
+		results.Add(new ValidationResult($"{nameof(Status)} cannot have the default value of {default(ScheduleStatus)}", new[] { nameof(Status) }));
+
+	  if (StartDateTime == default)
+		results.Add(new ValidationResult($"{nameof(StartDateTime)} is required", new[] { nameof(StartDateTime) }));
+
+	  if (EndDateTime == default)
+		results.Add(new ValidationResult($"{nameof(EndDateTime)} is required", new[] { nameof(EndDateTime) }));
+
+	  if (EndDateTime < StartDateTime)
+			results.Add(new ValidationResult($"{nameof(EndDateTime)} cannot be before {nameof(StartDateTime)}", new[] { nameof(StartDateTime), nameof(EndDateTime) }));
+
+	  return results;
+
+	}
   }
-
 }
