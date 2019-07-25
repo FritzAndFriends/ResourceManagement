345ed6f6300a2a82a891b0420436cd487f67076b
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-07-25 11:07:37 -0400

    Completed conversion to Preview 7 and now using sqlite

--- src/Fritz.ResourceManagement.Web/Startup.cs
+++ src/Fritz.ResourceManagement.Web/Startup.cs
@@ -32,17 +32,17 @@ namespace Fritz.ResourceManagement.Web
 		{
 			services.Configure<CookiePolicyOptions>(options =>
 			{
-			// This lambda determines whether user consent for non-essential cookies is needed for a given request.
-			options.CheckConsentNeeded = context => true;
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
 			});
 
 			services.AddDbContext<ApplicationDbContext>(options =>
-				options.UseSqlite("Data Source=resourcemgmt.db"));
+				options.UseMyDatabase(Configuration));
 			services.AddDefaultIdentity<MyUser>()
 				.AddEntityFrameworkStores<ApplicationDbContext>();
 
 			services.AddDbContext<Models.MyDbContext>(options =>
-				options.UseSqlite("Data Source=resourcemgmt.db"));
+				options.UseMyDatabase(Configuration));
 
 			services.AddSignalR();
 			services.AddServerSideBlazor();
