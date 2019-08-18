0ca564d9b5b2069956677c7e8457c6ccb9bc7e02
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-08-18 13:17:42 -0400

    Added unit test project and validation to save of availability

--- src/Fritz.ResourceManagement.WebClient/Startup.cs
+++ src/Fritz.ResourceManagement.WebClient/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Fritz.ResourceManagement.WebClient.Security;
+﻿using System.Net.Http;
+using Fritz.ResourceManagement.WebClient.Data;
+using Fritz.ResourceManagement.WebClient.Security;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,6 +15,16 @@ namespace Fritz.ResourceManagement.WebClient
 			// Auth
 			services.AddAuthorizationCore();
 			services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+
+			services.AddScoped<HttpClient>(s => {
+				var uriHelper = s.GetRequiredService<IUriHelper>();
+				return new HttpClient
+				{
+					BaseAddress = new System.Uri(uriHelper.GetBaseUri())
+				};
+			});
+
+			services.AddScoped<IScheduleRepository, ScheduleRepository>();
 
 			services.AddScoped<Data.ScheduleState>();
 			services.AddViewModels();
