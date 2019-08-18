65e5736ca7cc8277e3d39113f7699a78e3e575ee
Simon Geering <25039878+SimonGeering@users.noreply.github.com>
2019-08-18 19:54:17 +0100

    Migrated RecurrenceDataEntryViewModel to use ComponentBase #72

--- src/Fritz.ResourceManagement.WebClient/ViewModels/RecurrenceDataEntryViewModel.cs
+++ src/Fritz.ResourceManagement.WebClient/ViewModels/RecurrenceDataEntryViewModel.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Fritz.ResourceManagement.Domain;
 using Microsoft.AspNetCore.Components;
 
 namespace Fritz.ResourceManagement.WebClient.ViewModels
 {
-	public class RecurrenceDataEntryViewModel
+	public class RecurrenceDataEntryViewModel : ComponentBase
 	{
+		// Cheer 1000 cpayette 27/06/19
+		// Cheer 500 cpayette 8/08/19
+		
+		[Parameter]
 		public RecurringSchedule Schedule { get; set; }
 
+		[Parameter]
 		public EventCallback OnSave { get; set; }
 
 		public string Pattern { get; set; }
@@ -18,7 +24,8 @@ namespace Fritz.ResourceManagement.WebClient.ViewModels
 
 		public HashSet<string> DaysOfTheWeek { get; set; } = new HashSet<string>();
 
-
+		public TimeSpan EndTime { get; set; }
+		
 		public void OnDOWChange(string day)
 		{
 			if (this.DaysOfTheWeek.Contains(day))
@@ -41,6 +48,19 @@ namespace Fritz.ResourceManagement.WebClient.ViewModels
 			}
 		}
 
+		public async Task TimeChanged(UIChangeEventArgs args)
+		{
+			// TODO: Simon G - This was pulled in from the Code block in RecurrenceDataEntry.razor but does not appear to be called is it still needed?
+			if (EndTime != null)
+				return;
+
+			if (this.TimeOfDay > EndTime)
+			{
+				this.EndTime = this.EndTime.Add(TimeSpan.FromDays(1));
+			}
+			this.Schedule.Duration = this.EndTime.Subtract(this.TimeOfDay);
+		}
+		
 		private string CalculatedCronPattern()
 		{
 			var sb = new System.Text.StringBuilder();
